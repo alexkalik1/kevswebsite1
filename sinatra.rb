1ddf7e109e6cdc379892339a177953da64b55b42
alexkalik1 <alexanderk1996@gmail.com>
2018-06-30 12:42:30 -0400

    changing server

--- sinatra.rb
+++ sinatra.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
-# get '/' do
-#   send_file File.join(settings.public_folder, 'window_jackie.html')
-# end
+get '/' do
+  send_file File.join(settings.public_folder, 'ysk.html')
+end
